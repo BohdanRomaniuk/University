9afc8929b91286cdd311565f8de0297d08906bcb
Bohdan Romaniuk <broman@softserveinc.com>
2018-10-03 12:46:25 +0300

    Add DAL and BLL levels, Implement University Context

---
+++ DAL/Implementation/UniversityContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Implementation
+{
+    public class UniversityContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options):
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
