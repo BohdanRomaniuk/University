bee94f8e657393602d49ebadd1dc3e14147719f9
Bohdan Romaniuk <broman@softserveinc.com>
2018-10-03 12:51:17 +0300

    Add Repository Implementation

---
+++ DAL/Implementation/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DAL.Implementation
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        private readonly UniversityContext Context;
+
+        public Repository(UniversityContext context)
+        {
+            Context = context;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void Add(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void Remove(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Set<TEntity>().Update(entity);
+        }
+    }
+}
